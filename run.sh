6a24d595d47604a35dcd571dc61d2834b0389897
vivbigel <vivbigel@indiana.edu>
2019-07-14 23:14:52 +0000

    Marty Changes

--- run.sh
+++ run.sh
@@ -4,12 +4,12 @@ FORMAT=$3
 ID=$4
 EMAIL=$5
 
-export PATH="/data/linhai/bin/src/anaconda2/bin:/data/linhai/bin/src/bedtools2/bin:$PATH"
+export PATH="/var/www/html/data:$PATH"
 if [ ! -z "$EMAIL" ];then
     src/send_email.py start $ID $EMAIL
 fi
 
-regsnp_intron -f --iformat $FORMAT -s /data/linhai/bin/src/anaconda2/lib/python2.7/site-packages/regsnp_intron/settings/settings.json $INPUT $OUTPUT
+regsnp_intron -f --iformat $FORMAT -s  /home/larsmay/Muri/regsnp/regsnp_intron/regsnp_intron/settings/settings.json $INPUT $OUTPUT
 
 if [ ! -z "$EMAIL" ] && [ -s "$OUTPUT/snp.prediction.json" ];then
     src/send_email.py end $ID $EMAIL
--- submit.sh
+++ submit.sh
@@ -4,7 +4,7 @@ OUTPUT=$2
 LOG=$3
 SAMPLE=$INPUT
 WD=${LOG/log}
-SCRIPT="export PATH=/data/linhai/bin/src/anaconda2/bin:/data/linhai/bin/src/bedtools2/bin:$PATH; regsnp_intron -f -s /data/linhai/bin/src/anaconda2/lib/python2.7/site-packages/regsnp_intron/settings/settings.json $INPUT $OUTPUT > $LOG 2>&1"
+SCRIPT="export PATH=/var/www/html/data:$PATH; regsnp_intron -f -s /home/larsmay/Muri/regsnp/regsnp_intron/regsnp_intron/settings/settings.json $INPUT $OUTPUT > $LOG 2>&1"
 echo $SCRIPT > $SAMPLE.job
 qsub -l nodes=1:ppn=1,walltime=24:00:00 -d $PWD -o $WD -e $WD -M linhai@iupui.edu -m abe -N $SAMPLE $SAMPLE.job
 # rm $SAMPLE.job
