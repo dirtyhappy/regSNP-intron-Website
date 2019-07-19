ab4f24c9e58c7e479b2e678fc51e2f93f9f88394
vivbigel <vivbigel@indiana.edu>
2019-07-19 20:57:45 +0000

    Update repo

--- js/validate_form.js
+++ js/validate_form.js
@@ -1,7 +1,5 @@
 $(function(){
   document.getElementById('input_form').noValidate = true;
-  var valid = { };
-
 
 
 
@@ -54,65 +52,56 @@ $(function(){
 
 ////////////////////////////////////////////////////////////////////////////////
 
-	function validateFileContents (file){
-
-		var fileEl = document.getElementById("InputFile");
-		if (file === ""){
-			setErrorMessage(fileEl, "File is Empty");
-   valid.file = false;
-
-		}
-		if (/[^\d\w\są-ż]+/ig.test(file)){
-			setErrorMessage(fileEl, "Please upload valid input");
-   valid.file = false;
-
-		}
-
-		var lines = file.split('\n');
-		for (var i = 0; i < lines.length; i++){
-			var line = lines[i];
-			var strings = line.trim().split(/[\s ]+/);
-   if (strings[0] === ""){
-    continue;
-   }
-			if (strings.length != 4){
-				setErrorMessage(fileEl, "Input Must have 4 columns");
-    valid.file = false;
-
-			}
-			if (!/\b(\w*chr\w*)\b/.test(strings[0])){
-				setErrorMessage(fileEl, "First column must be chromosome.");
-    valid.file = false;
-
-			}
-			if (!/\d+/.test(strings[1])){
-				setErrorMessage(fileEl, "Second column must be position number.");
-    valdi.file = false;
-
-			}
-			if (!checkAlleles(strings[2])){
-			 setErrorMessage(fileEl, "S");
-
-			}
-			if(!checkAlleles(strings[3])){
-				setErrorMessage(fileEl, "Last column must be alternative allele");
-    valid.file = false;
-
-			}
-
-		}
-
-
-
-	}
+	// function validateFileContents (file){
+	// 	var fileEl = document.getElementById("InputFile");
+	// 	if (file === ""){
+	// 		setErrorMessage(fileEl, "File is Empty");
+	// 		return false;
+	// 	}
+	// 	if (/[^\d\w\są-ż]+/ig.test(file)){
+	// 		setErrorMessage(fileEl, "Please upload valid input");
+	// 		return false;
+	// 	}
+ //
+	// 	var lines = file.split('\n');
+	// 	for (var i = 0; i < lines.length; i++){
+	// 		var line = lines[i];
+	// 		var strings = line.trim().split(/[\s ]+/);
+ //   if (strings[0] === ""){
+ //    continue;
+ //   }
+	// 		if (strings.length != 4){
+	// 			setErrorMessage(fileEl, "Input Must have 4 columns");
+	// 			return false;
+	// 		}
+	// 		if (!/\b(\w*chr\w*)\b/.test(strings[0])){
+	// 			setErrorMessage(fileEl, "First column must be chromosome.");
+	// 			return false;
+	// 		}
+	// 		if (!/\d+/.test(strings[1])){
+	// 			setErrorMessage(fileEl, "Second column must be position number.");
+	// 			return false;
+	// 		}
+	// 		if (!checkAlleles(strings[2])){
+	// 		 setErrorMessage(fileEl, "S");
+	// 			return false;
+	// 		}
+	// 		if(!checkAlleles(strings[3])){
+	// 			setErrorMessage(fileEl, "Last column must be alternative allele");
+	// 			return false;
+	// 		}
+ //
+	// 	}
+ //
+	// 	return true;
+	// }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-  function validateTextInput(validObject){
+  function validateTextInput(){
 		var text = document.getElementById("TextArea");
     if (isEmpty(text)){
       //input is empty
-      validObject.text = true;
       return true;
     }
     else if (!/[^\d\w\są-ż]+/ig.test(text.value)){
@@ -125,38 +114,31 @@ $(function(){
           }
           if (strings.length != 4){
             setErrorMessage(text, 'Input must contain 4 columns : chromosome number, position, reference allele, and alternative allele ' + "invalid input at line: " + (i+1));
-            validObject.text = false;
             return false;
           }else {
 						if (!/\b(\w*chr\w*)\b/.test(strings[0])){
 							setErrorMessage(text, "First column must be chromosome. Invalid input at line "+ (i+1));
-							validObject.text = false;
-       return false;
+							return false;
 						}
 						if (!/\d+/.test(strings[1])){
 							setErrorMessage(text, "Second column must be position number. Invalid input at line "+ (i+1));
-							validObject.text = false;
-       return false;
+							return false;
 						}
 						if(checkAlleles(strings[2])){
 							if(!checkAlleles(strings[3])){
 								setErrorMessage(text, "Last column must be alternative allele. Invalid input at line "+ (i+1));
-								validObject.text = false;
-        return false;
+								return false;
 							}
 						}else{
 							setErrorMessage(text, "Second to last column must be reference allele. Invalid input at line "+ (i+1));
-							validObject.text = false;
-       return false;
+							return false;
 						}
 					}
 				}
-				valid.text = true;
-    return true;
+				return true;
       }
     else {
       setErrorMessage(text, "Please Enter Valid Input." );
-      validObject.text = false;
       return false;
     }
 
@@ -164,90 +146,66 @@ $(function(){
 
 ////////////////////////////////////////////////////////////////////////////////
 
-  function validateFile(validObject){
-
+  function validateFile(){
     var fileEl = document.getElementById('InputFile');
     var fileName = fileEl.value;
     if (isEmpty(fileEl)){
-      validObject.file = true;
       return true;
     }
-
     var selectorType = document.getElementById('SelectFormat').value;
     var allowedExtensions = ['txt', 'vcf', 'csv'];
     var fileExtension = fileName.split('.').pop().toLowerCase();
-
     for(var i = 0; i < allowedExtensions.length; i++){
       if(allowedExtensions[i] == fileExtension){
-       var file = fileEl.files[0];
-				   var reader = new FileReader();
-       reader.onload = function(e){
-         var text = e.target.result;
-         console.log(text);
-         validateFileContents(text);
-
-
-        };
-
-
-
-        reader.readAsText(file);
-        console.log(validObject.file);
-        if(validObject.file == false){
-          return false;
-        }
+				   // var contents = fileEl.files[0];
+				   // var reader = new FileReader();
+       // reader.onload = function(e){
+       //   var text = e.target.result;
+       //   if(!validateFileContents(text)){
+       //    valid.file = false;
+       //    showErrorMessage(fileEl);
+       //   }
         if (selectorType != fileExtension){
           setErrorMessage(document.getElementById('SelectFormat'), "Filetype must match selector");
-          validObject.selector = false;
           return false;
         }else{
-          validObject.file = true;
-          validObject.selector = true;
           return true;
         }
       }
     }
 
     setErrorMessage(fileEl, "Invalid File Type");
-    validObject.file = false;
     return false;
   }
 
-
 ////////////////////////////////////////////////////////////////////////////////
 
-  function validateDescription (validObject){
+  function validateDescription (){
     var description = document.getElementById("InputDescription");
     if (isEmpty(description)){
-      validObject.description = true;
       return true;
     }
     else if (!/[^\d\w\są-ż]+/ig.test(description.value)){
-      validObject.description = true;
       return true;
     }
     else {
       setErrorMessage(description, "Invalid Input");
-      validObject.description = false;
       return false;
     }
   }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-  function validateEmail(validObject){
+  function validateEmail(){
     var email = document.getElementById("InputEmail");
     if (isEmpty(email)){
-      validObject.email = true;
       return true;
     }
     else if (!/[^@]+@[^@]+/.test(email.value)){
       setErrorMessage(email, "Please enter a valid email address");
-      validObject.email = false;
       return false;
     }
     else {
-      validObject.email = true;
       return true;
     }
 
@@ -257,46 +215,47 @@ $(function(){
 
   //VALIDATION
   $('#input_form').on('submit', function(e){
-    e.preventDefault();
+   //e.preventDefault();
     var text = document.getElementById('TextArea');
     var file = document.getElementById("InputFile");
     var description = document.getElementById("InputDescription");
     var email = document.getElementById("InputEmail");
     var valid = { };
-
-
+    console.log(valid);
 
     var isFormValid;
 
-    if(!validateTextInput(valid)){
+    if(!validateTextInput()){
       showErrorMessage(text);
+      valid.text = false;
     }else{
       removeErrorMessage(text);
+      valid.text = true;
     }
 
-    if(!validateFile(valid)){
+    if(!validateFile()){
       showErrorMessage(file);
       showErrorMessage(document.getElementById('SelectFormat'));
-
+      valid.file = false;
     }else{
       removeErrorMessage(file);
-
+      valid.file = true;
     }
 
-    if(!validateDescription(valid)){
+    if(!validateDescription()){
       showErrorMessage(description);
-
+      valid.description = false;
     }else{
       removeErrorMessage(description);
-
+      valid.description = true;
     }
 
-    if(!validateEmail(valid)){
+    if(!validateEmail()){
       showErrorMessage(email);
-
+      valid.email = false;
     }else{
       removeErrorMessage(email);
-
+      valid.email = true;
     }
 
     if(isEmpty(document.getElementById("TextArea")) && isEmpty(document.getElementById("InputFile"))){
@@ -316,7 +275,7 @@ $(function(){
       }
       isFormValid = true;
     }
-    console.log(valid);
+
     if(!isFormValid){
       e.preventDefault();
     }
